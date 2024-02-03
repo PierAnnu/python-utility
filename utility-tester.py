f0d02591c5be60d829a603067c502e041004bec3
PierAnnu <worksofannu@gmail.com>
2024-02-03 17:42:05 +0100

    Structure Update
    Real structure adjustament

---
+++ utility-tester.py
@@ -0,0 +1,11 @@
+
+
+from utils.logger import Logger
+log = Logger("Utility Tester",4)
+
+log.hd("Hard Debug")
+log.d("Debug")
+log.i("info")
+log.w("warning")
+log.e("error")
+
--- utils/menu.py
+++ utils/menu.py
@@ -1,10 +1,12 @@
-
+from utils.logger import Logger
+log = Logger("Menu",4)
 
 
 class Menu:
     def __init__(self, description, functions):
+        log.hd("Menu Initialization")
         self.description = description
         self.functions = functions
         
     def run(self):
-        
+        log.hd("Menu Run")
